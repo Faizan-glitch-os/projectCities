a95534c8ae49895c0611b87202874e0436199d77
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-10-20 19:03:57 +0500

    pagination done

--- index.php
+++ index.php
@@ -4,5 +4,8 @@ require __DIR__ . '/inc/all.inc.php';
 
 $WorldCityRepository = new WorldCityRepository($pdo);
 
-$entries = $WorldCityRepository->fetch();
+$page = empty($_GET['page']) ? 1 : $_GET['page'];
+$limit = empty($_GET['limit']) ? 10 : $_GET['limit'];
+
+$entries = $WorldCityRepository->pagination($page, $limit);
 render('index', ['entries' => $entries]);
--- src/WorldCityRepository.php
+++ src/WorldCityRepository.php
@@ -23,11 +23,17 @@ class WorldCityRepository
         );
     }
 
-    // to fetch all models
-    public function fetch(): array
+    // to fetch all models with pagination
+    public function pagination(int $page = 1, int $limit = 10): array
     {
+        $page = max(1, $page);
+        $limit = max(10, $limit);
 
-        $stmt = $this->pdo->prepare("SELECT * FROM `worldcities` ORDER BY `population` DESC LIMIT 10");
+        $offset = ($page - 1) * $limit;
+
+        $stmt = $this->pdo->prepare("SELECT * FROM `worldcities` ORDER BY `population` DESC LIMIT :limit OFFSET :offset");
+        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
 
         $stmt->execute();
         $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
